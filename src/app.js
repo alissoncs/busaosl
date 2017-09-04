86ecf0e6ff3b90dbb38fc82ef4ef39c27d354ba4
Alisson da Silva <alisson.silva@4all.com>
2017-09-04 17:49:18 -0300

    sinoscap

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 var Promise = require('bluebird')
 const leopoldensefetchLines = require('./leopoldense/fetchLines')
+const leopoldensefetchLines = require('./sinoscap/fetchLines')
 
 leopoldensefetchLines()
